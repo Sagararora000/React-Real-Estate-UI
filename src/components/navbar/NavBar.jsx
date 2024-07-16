721ba0055af86a0b346b55ee25a46c40e96653ef
Sagar <sag28.01.2001@gmail.com>
2024-07-17 02:01:53 +0530

    navbar responsive completed

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.scss";
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <div className="NavBar">
@@ -26,17 +27,36 @@ const NavBar = () => {
         </div>
         <div className="right">
           <div className="profImage">
-            <img src="/favicon.png" alt="" srcset="" />
+            <img src="/favicon.png" alt="" srcset="" onclick />
             <span className="profText">John&nbsp;Doe</span>
           </div>
           <div className="profButton">
             <a href="">Profile</a>
           </div>
           <div className="menuIcon">
-            <img src="/menu.png" alt="" srcset="" onClick />
+            <img
+              src="/menu.png"
+              alt=""
+              srcset=""
+              onClick={() => {
+                setOpen(prev => !prev);
+                console.log(open);
+              }}
+            />
           </div>
-          <div className="drawer">
-            
+          <div className={open ? "active drawer" : "drawer"}>
+            <a href="" className="home">
+              Home
+            </a>
+            <a href="" className="about">
+              About
+            </a>
+            <a href="" className="contacts">
+              Contacts
+            </a>
+            <a href="" className="agents">
+              Agents
+            </a>
           </div>
         </div>
       </div>
